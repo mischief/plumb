6c2321edf071c960a8293baaced5bcdcf45a750d
mischief <mischief@offblast.org>
2013-12-10 19:14:34 -0800

    remove test which depended on outside interaction with plumber

--- plumb_test.go
+++ plumb_test.go
@@ -1,26 +1,43 @@
 package plumb
 
 import (
+	"bytes"
+	"code.google.com/p/goplan9/plan9"
 	"testing"
 )
 
-func TestPlumbRecv(t *testing.T) {
-	pl, err := PlumbOpen("test", 0)
+func TestPlumbSendRecv(t *testing.T) {
+	pls, err := PlumbOpen("send", plan9.OWRITE)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	for {
-		msg, err := pl.Recv()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		t.Logf("%+v", msg)
-
-		if msg.Attr["exit"] == "true" {
-			break
-		}
+	pl, err := PlumbOpen("edit", plan9.OREAD)
+	if err != nil {
+		t.Fatal(err)
 	}
 
+	pls.Send(&PlumbMsg{Src: "plumb_test", Dst: "edit", Wdir: "/tmp", Type: "text", Data: []byte("foo.c")})
+
+	msg, err := pl.Recv()
+
+	t.Logf("%+v", msg)
+
+	if err != nil {
+		t.Fatalf("recv: %s", err)
+	}
+
+	if msg.Src != "plumb_test" {
+		t.Fatal("recv src")
+	}
+	if msg.Dst != "edit" {
+		t.Fatal("recv dst")
+	}
+	if msg.Wdir != "/tmp" {
+		t.Fatal("recv wdir")
+	}
+	if !bytes.Equal(msg.Data, []byte("foo.c")) {
+		t.Logf("%q %X", msg.Data, msg.Data)
+		t.Fatal("recv data")
+	}
 }
